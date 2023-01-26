7d8944a9f0e98eb6c1712d61170ae438e7a0c02c
f3rnn <fvvinhas@gmail.com>
2023-01-26 18:04:01 +0000

    arquivos alterados em develop

--- gasolina.py
+++ gasolina.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 csv = pd.read_csv('gasolina.csv')
 
-with sns.axes_style('darkgrid'):
+with sns.axes_style('whitegrid'):
   grafico = sns.lineplot(data = csv, x='dia', y='venda')
-  grafico.set(title='Preço médio por dia da gasolina em São Paulo (jul/21)', xlabel = 'Dia', ylabel = 'Preço (R$)')
+  grafico.set(title='Preço médio por dia da gasolina em São Paulo', xlabel = 'Dias', ylabel = 'Preço em reais')
 
 grafico.figure.savefig("./gasolina.png")
